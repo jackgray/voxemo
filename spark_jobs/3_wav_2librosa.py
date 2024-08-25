88eb46697e639aa7079a81c86fe6868366c16a15
jackgray <jgrayau@gmail.com>
2024-08-25 11:52:16 -0400

    removing files

--- spark_jobs/3_wav_2librosa.py
+++ spark_jobs/3_wav_2librosa.py
@@ -3,11 +3,12 @@ import librosa
 import numpy as np
 
 # Load audio file
-y, sr = librosa.load('path/to/audiofile.wav', sr=None)  # sr=None retains the original sampling rate
+y, sr = librosa.load(audiofile_path, sr=None)  # sr=None retains the original sampling rate
 
 # Extract MFCC features
 mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)  # Example: 13 MFCCs
 
 # Extract Chroma features
 chroma = librosa.feature.chroma_stft(y=y, sr=sr)
+
 
